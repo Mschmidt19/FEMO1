f3bb253e94f3af622b1922e09075950c0d8e1412
Olive B <oliveb@Olives-MacBook-Pro.local>
2018-09-25 14:13:11 +0100

    all questions now added

--- Femo1/QuestionScene/QuestionClasses/QuestionList.swift
+++ Femo1/QuestionScene/QuestionClasses/QuestionList.swift
@@ -42,6 +42,36 @@ class QuestionList {
         
         list.append(Question(questionText: "In SQL, what would the 'FOREIGN KEY' command do?", choiceA: "Link two tables together", choiceB: "Delete a table", choiceC: "Create a new key", choiceD: "Link two keys together", answer: 1))
         
+        list.append(Question(questionText: "Which of the following is NOT a backend language?", choiceA: "CSS", choiceB: "Java", choiceC: "Ruby", choiceD: "Perl", answer: 1))
+        
+        list.append(Question(questionText: "In the command line, what does 'cat' do?", choiceA: "String1", choiceB: "String2", choiceC: "String3", choiceD: "String4", answer: 1))
+        
+        list.append(Question(questionText: "In the command line, what does 'cd..' do?", choiceA: "List directories", choiceB: "Make a new directory", choiceC: "Move up one directory", choiceD: "List the contents of a file", answer: 3))
+        
+        list.append(Question(questionText: "In the command line, which command makes a new directory?", choiceA: "echo", choiceB: "mkdir", choiceC: "tail", choiceD: "curl", answer: 2))
+        
+        list.append(Question(questionText: "Which command on a mac keyboard will open the console?", choiceA: "Shift + cmd + H", choiceB: "Shift + cmd + A", choiceC: "cmd + J", choiceD: "alt + cmd + J", answer: 4))
+        
+        list.append(Question(questionText: "What percentage of tech startups fail within their first year?", choiceA: "29%", choiceB: "76%", choiceC: "90%", choiceD: "50%", answer: 3))
+        
+        list.append(Question(questionText: "In 2017, which of the following frameworks was the most commonly used?", choiceA: "React", choiceB: "Angular JS", choiceC: ".Net Core", choiceD: "Node.js", answer: 1))
+        
+        list.append(Question(questionText: "In 2017, which of the following languages was the least commonly used?", choiceA: "Coffeescript", choiceB: "Perl", choiceC: "Haskell", choiceD: "Groovy", answer: 4))
+        
+        list.append(Question(questionText: "What is PHP short for?", choiceA: "Personal Hypertext Page", choiceB: "Personal Home Page", choiceC: "Programming Hypertext Page", choiceD: "Programme Home Page", answer: 2))
+        
+        list.append(Question(questionText: "IP Address. What does IP stand for?", choiceA: "Internet Protocol", choiceB: "Information Packet", choiceC: "Internet Page", choiceD: "Incoming Page", answer: 2))
+        
+        list.append(Question(questionText: "In all programming languages, what does 'int' stand for?", choiceA: "Internet", choiceB: "Integer", choiceC: "Initialize", choiceD: "Internal", answer: 2))
+        
+        list.append(Question(questionText: "What symbol in Javascript seperates statements?", choiceA: "String1", choiceB: "String2", choiceC: "String3", choiceD: "String4", answer: 1))
+        
+        list.append(Question(questionText: "What does the symbol modulo do?", choiceA: "Adds two integers together", choiceB: "Multiplies an array", choiceC: "Gives you the total", choiceD: "Finds the remainder", answer: 4))
+        
+        list.append(Question(questionText: "What of the below is NOT a database?", choiceA: "SQL", choiceB: "Firebase", choiceC: "RSpec", choiceD: "PSQL", answer: 3))
+        
+        list.append(Question(questionText: "Which method would correct this array alphabetically? [F, E, J, O, A, Q]", choiceA: ".length", choiceB: ".trim", choiceC: ".toLowerCase", choiceD: ".sort", answer: 4))
+        
         
 
     }
