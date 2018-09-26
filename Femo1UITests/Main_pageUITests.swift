45191f70585de1f615d8b437b4efe73e961f9b24
Marek <MarekSchmidt@outlook.com>
2018-09-26 16:47:29 +0100

    Two UI Tests for Main_page added

--- Femo1UITests/Main_pageUITests.swift
+++ Femo1UITests/Main_pageUITests.swift
@@ -1,30 +1,38 @@
 //
-//  Femo1UITests.swift
+//  Main_pageUITests.swift
 //  Femo1UITests
 //
-//  Created by Farah Jabri on 25/09/2018.
+//  Created by Marek Schmidt on 9/26/18.
 //  Copyright © 2018 FEMO@Makers. All rights reserved.
 //
 
 import XCTest
 
-class Femo1UITests: XCTestCase {
-        
+class Main_pageUITests: XCTestCase {
+
+    //
+    //  Femo1UITests.swift
+    //  Femo1UITests
+    //
+    //  Created by Farah Jabri on 25/09/2018.
+    //  Copyright © 2018 FEMO@Makers. All rights reserved.
+    //
+    
     override func setUp() {
         super.setUp()
-    
+        
         // Put setup code here. This method is called before the invocation of each test method in the class.
         
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
         
-//        XCUIApplication().launch()
+        //        XCUIApplication().launch()
         
         let app = XCUIApplication()
         app.launchArguments = ["enable-testing"]
         app.launch()
-
+        
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
     
@@ -33,5 +41,17 @@ class Femo1UITests: XCTestCase {
         super.tearDown()
     }
     
-
+    func testResumeButtonIsHittable() {
+        let app = XCUIApplication()
+        let newGameButton = app.otherElements["Resume"]
+        XCTAssertTrue(newGameButton.isHittable)
+    }
+    
+    func testNewGameButtonIsHittable() {
+        let app = XCUIApplication()
+        let newGameButton = app.otherElements["New Game"]
+        XCTAssertTrue(newGameButton.isHittable)
+    }
+        
 }
+
